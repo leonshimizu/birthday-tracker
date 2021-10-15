02afa29726965420a3c4d07e470f7bb8454e115b
“leonshimizu” <lmshimizu@gmail.com>
2021-10-14 20:27:05 -0700

    birthdays create

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -1,7 +1,20 @@
 class BirthdaysController < ApplicationController
   
   def index
-    render json: {message: "hello"}
+    birthdays = Birthday.all
+    render json: birthdays.as_json
   end
     
+  def create
+    new_birthday = Birthday.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      day: params[:day],
+      month: params[:month],
+      year: params[:year]
+    )
+    new_birthday.save
+    render json: new_birthday.as_json
+  end
+
 end
